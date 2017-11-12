cd6d76529147b0369b6f422b2a7fd595f16578b9
David Welch <dmw9400@gmail.com>
2017-11-12 19:41:42 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   return_array = []
   while i < collection.length
     if yield(collection[i]) == true
-      return_array.push(yield(collection[i]))
+      return_array.push(collection[i])
     end 
     i+=1
   end
